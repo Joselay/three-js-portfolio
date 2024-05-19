599e0ec6b911c989de4e5ef097f8cd316d9a1bb4
Joselay <tongmenglaysmae5@gmail.com>
2024-05-20 00:25:16 +0700

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import {
+  Text,
   Html,
   ContactShadows,
   Environment,
@@ -27,6 +28,15 @@ const App = () => {
         snap={{ mass: 4, tension: 400 }}
       >
         <Float rotationIntensity={0.4}>
+          <rectAreaLight
+            width={2.5}
+            height={1.65}
+            intensity={65}
+            color={"#161513"}
+            rotation={[0.1, Math.PI, 0]}
+            position={[0, 0.55, -1.15]}
+          />
+
           <primitive
             object={computer.scene}
             position-y={-1.2}
@@ -39,9 +49,19 @@ const App = () => {
               position={[0, 1.56, -1.4]}
               rotation-x={-0.256}
             >
-              <iframe src="https://menglay-portfolio.surge.sh/" />
+              <iframe src="https://menglay-react-portfolio.vercel.app/" />
             </Html>
           </primitive>
+          <Text
+            font="./bangers-v20-latin-regular.woff"
+            fontSize={0.55}
+            position={[2, 0.75, 0.75]}
+            rotation-y={-1.25}
+            maxWidth={2}
+            textAlign="center"
+          >
+            SMAE TONGMENGLAY
+          </Text>
         </Float>
       </PresentationControls>
 
